b033106a2f020a73cba2c3d03fd9f15c772b3c80
AndSchool <andrewc477@nycstudents.net>
2025-11-25 08:46:25 -0500

    update test code

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         CorruptWord weird6 = new CorruptWord("epicword");
         //it's a word, so it won't take spaces
+        System.out.println(weird6.Corrupting());
         System.out.println(weird6.Encrypt());
         System.out.print(weird6.Decrypt("LWPJDVYK", 7));
     }
